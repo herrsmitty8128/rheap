dfa717407c2d5dfba18322ea1f794a36325ec417
herrsmitty8128 <16559135+herrsmitty8128@users.noreply.github.com>
2023-05-12 21:37:41 -0400

    updates 2023-05-12 2137

---
+++ docs/implementors/rheap/trait.Heap.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"rheap":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
